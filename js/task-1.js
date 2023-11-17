8652a23208218b4f756cb0f01ede4a2f968fb1b1
peychma <mariapei4eva@gmail.com>
2023-11-17 22:49:02 +0200

    .

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,9 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function isEnoughCapacity(products, containerSize) {
+    const quantities = Object.values(products);
+    const totalQuantity = quantities.reduce((sum, quantity) => sum + quantity, 0);
+    return totalQuantity <= containerSize;
 }
-
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,18 @@
-function getElementWidth(content, padding, border) {
-return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-}
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+const profile = {
+  username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+      return `${this.username} has ${this.playTime} active hours!`
+  },
+};
+console.log(profile.getInfo());
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
